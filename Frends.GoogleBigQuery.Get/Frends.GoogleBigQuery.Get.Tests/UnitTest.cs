be313fbcfd78d4fab4508ce4113164d15fb305fc
ttossavainen <teemu.tossavainen@hiq.fi>
2023-08-31 12:57:00 +0300

    init

---
+++ Frends.GoogleBigQuery.Get/Frends.GoogleBigQuery.Get.Tests/UnitTest.cs
@@ -0,0 +1,118 @@
+using Frends.GoogleBigQuery.Get.Definitions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace Frends.GoogleBigQuery.Get.Tests;
+
+[TestClass]
+public class UnitTest
+{
+    private Connection _connection = new();
+    private Options _options = new();
+    private readonly string? _secretJson = Environment.GetEnvironmentVariable("BigQuery_SecretJson");
+
+    [TestInitialize]
+    public void Init()
+    {
+        string json;
+
+        // This one is to handle CI secret problems.
+        if (File.Exists(_secretJson))
+            json = File.ReadAllText(_secretJson);
+        else
+        {
+            var _json = !string.IsNullOrEmpty(_secretJson) ? _secretJson : "";
+            byte[] bytes = Convert.FromBase64String(_json);
+            json = Encoding.UTF8.GetString(bytes);
+        }
+
+        _connection = new()
+        {
+            ReadJsonMethod = ReadJsonMethods.JSON,
+            Resource = Resources.Datasets,
+            SecretJson = json,
+            ProjectId = "instant-stone-387712",
+            DatasetId = "tasktest",
+            TableId = "testtable",
+            BaseUri = null,
+            CredentialsFilePath = null,
+            JobId = null,
+            ModelId = null,
+            ReadMask = null,
+            RoutineId = null,
+            SelectedFields = null,
+            View = Views.Storagestats
+        };
+
+        _options = new()
+        {
+            ThrowOnError = true,
+        };
+    }
+
+    [TestMethod]
+    public async Task Get_Success_Datasets()
+    {
+        var result = await GoogleBigQuery.Get(_connection, _options, default);
+        Assert.IsTrue(result.Success);
+        Assert.IsNotNull(result.Data);
+        Assert.IsNull(result.ErrorMessage);
+    }
+
+    [TestMethod]
+    public async Task Get_Success_Tables()
+    {
+        _connection.Resource = Resources.Tables;
+        var result = await GoogleBigQuery.Get(_connection, _options, default);
+        Assert.IsTrue(result.Success);
+        Assert.IsNotNull(result.Data);
+        Assert.IsNull(result.ErrorMessage);
+    }
+
+    [TestMethod]
+    public async Task Get_Success_Tables_SelectedFields()
+    {
+        _connection.Resource = Resources.Tables;
+        _connection.SelectedFields = ("age,country");
+        var result = await GoogleBigQuery.Get(_connection, _options, default);
+        Assert.IsTrue(result.Success);
+        Assert.IsNotNull(result.Data);
+        Assert.IsNull(result.ErrorMessage);
+    }
+
+    [TestMethod]
+    public async Task Get_Invalid_SecretJson_Throw()
+    {
+        _connection.SecretJson = @"{ 
+""Foo"": ""Bar""
+}";
+        var ex = await Assert.ThrowsExceptionAsync<Exception>(() => GoogleBigQuery.Get(_connection, _options, default));
+        Assert.IsNotNull(ex);
+        Assert.AreEqual("Error occured: Error creating credential from JSON or JSON parameters. Unrecognized credential type .", ex.Message);
+    }
+
+    [TestMethod]
+    public async Task Get_Invalid_SecretJson_ReturnError()
+    {
+        _connection.SecretJson = @"{ 
+""Foo"": ""Bar""
+}";
+
+        _options.ThrowOnError = false;
+
+        var result = await GoogleBigQuery.Get(_connection, _options, default);
+        Assert.IsFalse(result.Success);
+        Assert.IsNull(result.Data);
+        Assert.AreEqual("Error occured: Error creating credential from JSON or JSON parameters. Unrecognized credential type .", result.ErrorMessage);
+    }
+
+    [TestMethod]
+    public async Task Get_Success_BaseUri()
+    {
+        _connection.BaseUri = "https://bigquery.googleapis.com/bigquery/v2/";
+        var result = await GoogleBigQuery.Get(_connection, _options, default);
+        Assert.IsTrue(result.Success);
+        Assert.IsNotNull(result.Data);
+        Assert.IsNull(result.ErrorMessage);
+    }
+}
---
+++ Frends.GoogleBigQuery.Get/Frends.GoogleBigQuery.Get/Definitions/Result.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Frends.GoogleBigQuery.Get.Definitions;
+
+/// <summary>
+/// Task's result.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// Operation complete without errors.
+    /// </summary>
+    /// <example>true</example>
+    public bool Success { get; private set; }
+
+    /// <summary>
+    /// Request content as Get of DataSets.
+    /// </summary>
+    /// <example>{{	"access": [ { "dataset": null, "domain": null, "groupByEmail": null ...	}}</example>
+    public JToken Data { get; private set; }
+
+    /// <summary>
+    /// Error message.
+    /// </summary>
+    /// <example>Error occured...</example>
+    public string ErrorMessage { get; private set; }
+
+    internal Result(bool success, JToken data, string errorMessage)
+    {
+        Success = success;
+        Data = data;
+        ErrorMessage = errorMessage;
+    }
+}

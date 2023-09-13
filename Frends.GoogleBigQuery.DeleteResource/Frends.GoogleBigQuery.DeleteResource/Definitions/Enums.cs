b3f0625f3babd8504e877147414704d7131c2ea8
ttossavainen <teemu.tossavainen@hiq.fi>
2023-09-13 12:38:48 +0300

    init

---
+++ Frends.GoogleBigQuery.DeleteResource/Frends.GoogleBigQuery.DeleteResource/Definitions/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace Frends.GoogleBigQuery.DeleteResource.Definitions;
+#pragma warning disable CS1591 // Self explanatory
+
+/// <summary>
+/// Resources.
+/// </summary>
+public enum Resources
+{
+    Dataset,
+    Job,
+    Model,
+    Routine,
+    Table
+}
+
+/// <summary>
+/// Method to read JSON credentials.
+/// </summary>
+public enum ReadJsonMethods
+{
+    JSON,
+    File,
+}
